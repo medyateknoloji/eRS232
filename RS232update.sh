d140153a141461c99026cde4a04d727e0f39fd55
tayfunerkorkmaz <tayfunerkorkmaz@gmail.com>
2017-10-13 11:09:16 +0300

    Add files via upload

---
+++ RS232update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Guncelleme basliyor..."
+sudo rm /boot/cmdline.txt
+echo "cmdline.txt silindi"
+sudo touch /boot/cmdline.txt
+echo "cmdline.txt olusturuldu"
+sudo sh -c "echo 'consoleblank=1 logo.nologo dwc_otg.lpm_enable=0 console=tty1 root=PARTUUID=84fa8189-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait  quiet   plymouth.ignore-serial-consoles
+ loglevel=1 quiet' >> /boot/cmdline.txt"
+echo "cmdline.txt duzenlendi"
+sudo echo "enable_uart=1" >> /boot/config.txt
+echo "config.txt duzenlendi"
+echo "pyserial kutuphanesi kuruluyor..."
+sudo python -m pip install pyserial
+cp ../RS232/TVon.py ../TVon.py
+cp ../RS232/TVoff.py ../TVoff.py
+echo "Dosyalar kopyalandi..."
+echo "crontab ekleniyor..."
+sudo touch /var/spool/cron/crontabs/pi
+sudo echo "# m h  dom mon dow   command" >> /var/spool/cron/crontabs/pi
+sudo echo "@reboot python /home/pi/TVon.py " >> /var/spool/cron/crontabs/pi
+sudo echo "00 04 * * * sudo reboot " >> /var/spool/cron/crontabs/pi
+sudo reboot
+
+
+
+
+
